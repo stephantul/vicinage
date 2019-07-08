ebc793b8696b8234c8884e3ea0c9656e481f2259
stephantul <stephan.tulkens@uantwerpen.be>
2019-07-08 16:05:14 +0200

    add rn

---
+++ vicinage/rn.py
@@ -0,0 +1,16 @@
+"""Radius-based distance."""
+import numpy as np
+from sklearn.metrics import pairwise_distances
+
+
+def r_subloop(X,
+              Y,
+              function="cosine",
+              radius=1):
+    """Calculate distance from each word in word_a to each word in word_b."""
+    dist_mtr = pairwise_distances(X, Y, metric=function)
+    results = []
+    for x in radius:
+        results.append(np.sum(dist_mtr < x, 1))
+
+    return results
