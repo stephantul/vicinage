d3b1ff814751cbdeba5f306204c31a02a74dc69c
stephantul <stephan.tulkens@uantwerpen.be>
2019-07-08 16:26:43 +0200

    add default args, correct argument order

--- vicinage/rd.py
+++ vicinage/rd.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.metrics import pairwise_distances
 
 
-def rd_subloop(X, Y, metric, n):
+def rd_subloop(X, Y, n, metric="cosine"):
     """Separate method because sometimes we want to analyze the matrix."""
     if X is Y:
         dists = pairwise_distances(X, metric=metric)
